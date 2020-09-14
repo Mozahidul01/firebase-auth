1dc6a11572565c45346eea923416dc71fb7a20fe
mozahidul01 <mozo.postbox@gmail.com>
2020-09-15 02:28:12 +0600

    Sign In with Google auth done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user, setUser] = useState({
+      isSignedIn: false,
+      name: '',
+      email: '',
+      photo: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, email, photoURL} = res.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(signedInUser);
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+    })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photo: ''
+        }
+        setUser(signedOutUser);
+      })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" >
+      {
+        user.isSignedIn ? <button className="btn" onClick={handleSignOut}>Sign Out</button> 
+        : <button className="btn" onClick={handleSignIn}>Sign In With Google</button>
+      }
+
+      {
+        user.isSignedIn && <div>
+           <h2>Welcome, {user.name}</h2>
+           <img src={user.photo} alt=""></img>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAfI-d3m4wL4QcPvzWy9qA7XbPP1f0xAU",
+    authDomain: "ema-john-demo.firebaseapp.com",
+    databaseURL: "https://ema-john-demo.firebaseio.com",
+    projectId: "ema-john-demo",
+    storageBucket: "ema-john-demo.appspot.com",
+    messagingSenderId: "483892473879",
+    appId: "1:483892473879:web:757336b0c9efa6e8ba6c52"
+  };
+
+  export default firebaseConfig;
